c8232f66a165eeb48ccc533e8eb0427dafb8dfdf
zeez09 <harunaabdulazeez766@gmail.com>
2025-10-07 15:28:36 +0100

    designed the cards and finance pages

--- app/(tabs)/cards.tsx
+++ app/(tabs)/cards.tsx
@@ -1,9 +1,431 @@
-import { Text, View } from "react-native";
+import { FontAwesome6 } from '@expo/vector-icons';
+import { ShieldCheck, Zap } from 'lucide-react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import CheckBox from 'react-native-check-box';
 
 export default function Cards() {
+  const [activeTab, setActiveTab] = useState("virtual");
+
+  const [checked, setChecked] = useState(false);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Rewards Screen 🎁</Text>
-    </View>
+    <SafeAreaView style={Styles.page}>
+      <View style={Styles.container}>
+
+        <View style={Styles.headerTab}>
+          <Text style={Styles.headerTextLeft}>OPay Cards</Text>
+          <Text style={Styles.headerTextRight}>Q&A</Text>
+        </View>
+
+        
+        <View style={Styles.heroTab}>
+          <TouchableOpacity onPress={() => setActiveTab("virtual")}>
+            <View style={Styles.tabItem}>
+              <Text
+                style={[
+                  Styles.heroTabText,
+                  activeTab === "virtual" && Styles.activeText,
+                ]}
+              >
+                Virtual Card
+              </Text>
+              {activeTab === "virtual" && <View style={Styles.shortBorder} />}
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setActiveTab("physical")}>
+            <View style={Styles.tabItem}>
+              <Text
+                style={[
+                  Styles.heroTabText,
+                  activeTab === "physical" && Styles.activeText,
+                ]}
+              >
+                Physical Card
+              </Text>
+              {activeTab === "physical" && <View style={Styles.shortBorder} />}
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView contentContainerStyle={Styles.scrollContainer} showsVerticalScrollIndicator={false}>
+
+        {activeTab === "virtual" ? (
+          
+          
+          <View style={Styles.cardTab}>
+            <View style={Styles.cardTop}>
+              <Text style={Styles.txt}>OPay</Text>
+              <View style={Styles.bottomTxt}>
+                <Text style={Styles.txtLeft}>Virtual Card</Text>
+                <Text style={Styles.txtRight}>Verve</Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <Zap color="#059c50ff" size={22} fill="#059c50ff" />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Instant Access</Text>
+                <Text style={Styles.iconTxt2}>
+                  Apply and activate <Text style={Styles.iconTxt3}>Instantly</Text>
+                </Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="sack-dollar" color="#059c50ff" size={22} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Limited-time cashback</Text>
+                <Text style={Styles.iconTxt2}>
+                  win up to{" "}
+                  <Text style={Styles.iconTxt3}>
+                    10% cashback on card transactions
+                  </Text>
+                  {"\n"}before Oct 31, 2025
+                </Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="earth-africa" color="#059c50ff" size={22} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Rep Your State Of Origin</Text>
+                <Text style={Styles.iconTxt3}>
+                  Get a virtual OPay card unique to your state {"\n"}of origin
+                </Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="globe" color="#059c50ff" size={22} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Online Merchant Acceptance</Text>
+                <Text style={Styles.iconTxt2}>Accepted by <Text style={Styles.iconTxt3}>40,000+</Text> online merchants{"\n"}including JUMIA, KONGA, NETFLIX UBER{"\n"}Wallet Funding and others</Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <ShieldCheck  color="#059c50ff" fill="green" stroke={"white"} size={32} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Security</Text>
+                <View style={Styles.lastTxt}>
+                <Text style={Styles.iconTxt3}>CBN</Text><Text style={Styles.iconTxt2}>
+                    licensed,
+                  </Text><Text style={Styles.iconTxt3}>NDIC</Text><Text style={Styles.iconTxt2}>
+                    Insured
+                  </Text>
+                  </View>
+              </View>
+            </View>
+
+          </View>
+        ) : (
+          <View style={Styles.cardTab}>
+            <View style={Styles.cardTop1}>
+              <Text style={Styles.txt}>OPay</Text>
+              <Text style={Styles.txtRight1}>Verve</Text>
+              </View>
+            
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="credit-card" color="#059c50ff" size={22} fill="#059c50ff" />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Free Application and Usage</Text>
+                <View style={Styles.iconTxt1}>
+
+                <Text style={Styles.iconTxt3}>
+                  Free </Text><Text style={Styles.iconTxt2}>Application,</Text>
+                  <Text style={Styles.iconTxt3}>
+                  Zero </Text><Text style={Styles.iconTxt2}>maintenance</Text></View>
+                
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="sack-dollar" color="#059c50ff" size={22} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Limited-time cashback</Text>
+                <Text style={Styles.iconTxt2}>
+                  win up to{" "}
+                  <Text style={Styles.iconTxt3}>
+                    10% cashback on card transactions
+                  </Text>
+                  {"\n"}before Oct 31, 2025
+                </Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="earth-africa" color="#059c50ff" size={22} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Rep Your State Of Origin</Text>
+                <Text style={Styles.iconTxt3}>
+                  Get a virtual OPay card unique to your state {"\n"}of origin
+                </Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <FontAwesome6 name="globe" color="#059c50ff" size={22} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Online Merchant Acceptance</Text>
+                <Text style={Styles.iconTxt2}>Accepted by <Text style={Styles.iconTxt3}>40,000+</Text> online merchants{"\n"}including JUMIA, KONGA, NETFLIX UBER{"\n"}Wallet Funding and others</Text>
+              </View>
+            </View>
+
+            <View style={Styles.bottomCardTab1}>
+              <View style={Styles.iconDiv}>
+                <ShieldCheck  color="#059c50ff" fill="green" stroke={"white"} size={32} />
+              </View>
+              <View style={Styles.iconDivTxt}>
+                <Text style={Styles.iconTxt}>Security</Text>
+                <View style={Styles.lastTxt}>
+                <Text style={Styles.iconTxt3}>CBN</Text><Text style={Styles.iconTxt2}>
+                    licensed,
+                  </Text><Text style={Styles.iconTxt3}>NDIC</Text><Text style={Styles.iconTxt2}>
+                    Insured
+                  </Text>
+                  </View>
+              </View>
+            </View>
+
+          </View>
+        )}
+        </ScrollView>
+
+        <View style={Styles.bottomBtnTab}>
+          <View style={Styles.topInner}>
+          <CheckBox
+        isChecked={checked}
+        onClick={() => setChecked(!checked)}
+        checkedCheckBoxColor="#4CAF50"  
+        uncheckedCheckBoxColor="#aaa" 
+      />
+      <Text style={Styles.bottomTxt2}>Click the button below to accept <Text style={Styles.iconTxt3}>Terms &{"\n"}Conditions</Text></Text>
+      </View>
+      
+
+        </View>
+        <TouchableOpacity style={Styles.bottomBtn}>
+                <Text style={Styles.bottomBtnText}>Get It Now</Text>
+              </TouchableOpacity>
+
+
+      </View>
+    </SafeAreaView>
   );
 }
+
+const Styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 16,
+    paddingTop: 20,
+  },
+  container: {
+    flex: 1,
+  },
+  headerTab: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTextLeft: {
+    fontSize: 24,
+    fontWeight: "600",
+  },
+  headerTextRight: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "green",
+  },
+  heroTab: {
+    marginTop: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+  },
+  tabItem: {
+    alignItems: "center",
+  },
+  heroTabText: {
+    fontSize: 18,
+    color: "#ccc",
+  },
+  activeText: {
+    color: "black",
+  },
+  shortBorder: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#ccc",
+    width: 40, 
+    marginTop: 5,
+    borderRadius: 1,
+  },
+  cardTab: {
+    marginTop: 30,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 10,
+    padding: 20,
+    width: "100%",
+    flexDirection: "column",
+    marginVertical: 20,
+    gap: 15,
+  },
+  cardTop: {
+    height: 200,
+    backgroundColor: "#25f68dff",
+    borderRadius: 10,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  txt: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "600",
+    margin: 20,
+  },
+  bottomTxt: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  txtLeft: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "300",
+  },
+  txtRight: {
+    color: "#252cf6ff",
+    fontSize: 24,
+    fontWeight: "600",
+  },
+  bottomCardTab1: {
+    flexDirection: "row",
+    gap: 15,
+  },
+  iconDiv: {
+    height: 50,
+    width: 50,
+    backgroundColor: "#d9d9d9",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconDivTxt: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  iconTxt: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  iconTxt2: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  iconTxt3: {
+    fontSize: 14,
+    color: "green",
+    fontWeight: 700,
+  },
+
+  scrollContainer: {
+    paddingBottom: 20,
+  },
+
+  lastTxt:{
+    flexDirection: "row",
+    gap: 4,
+
+  },
+
+  bottomBtnTab:{
+    backgroundColor: "white",
+    padding: 10,
+    alignItems: "center",
+    alignSelf: "center",
+    width: "100%",
+    marginTop: 30,
+    
+    
+
+  },
+
+  topInner:{
+    flexDirection:"row",
+    gap: 10,
+
+  },
+
+  bottomTxt2:{
+    fontWeight: 600,
+    color: "#686666ff"
+
+
+  },
+
+
+  bottomBtn: {
+    backgroundColor: "#09f768ff",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 30,
+    alignSelf: "center",
+    width: "90%",
+    margin: 15,
+  },
+
+  bottomBtnText: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "700",
+  },
+
+  cardTop1:{
+    flexDirection: "row",
+    height: 200,
+    backgroundColor: "#25f68dff",
+    borderRadius: 10,
+    justifyContent: "space-between",
+    padding: 10,
+
+  },
+
+  txtRight1:{
+    color: "blue",
+    fontSize: 24,
+    fontWeight: "600",
+    margin: 20,
+  },
+
+  iconTxt1:{
+    flexDirection:"row",
+    gap: 4,
+
+  },
+});
--- app/(tabs)/finance.tsx
+++ app/(tabs)/finance.tsx
@@ -1,51 +1,120 @@
-import { Bolt, Wallet } from "lucide-react-native";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { Bolt, CircleDollarSign, Wallet } from "lucide-react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Finance() {
   return (
     <SafeAreaView style={Styles.page}>
+      <ScrollView contentContainerStyle={Styles.scrollContainer} showsVerticalScrollIndicator={false}>
       <View style={Styles.container}>
         <View style={Styles.headerTab}>
           <Text style={Styles.firstText}>Finance</Text>
           <Text>from BlueRidge Microfinance Bank</Text>
-          <Bolt/>
-
-
+          <Bolt />
         </View>
-
-        <View style={Styles.balaceContainer}>
+        <View style={Styles.balanceContainer}>
           <View style={Styles.leftContent}>
             <Text>Total Balance</Text>
             <Text style={Styles.star}>⁎⁎⁎⁎</Text>
-            
-            
-
           </View>
+
           <View style={Styles.rightContent}>
-            <Text>Total interest</Text>
+            <Text>Total Interest</Text>
             <Text style={Styles.starRight}>⁎⁎⁎⁎</Text>
-
           </View>
-          
         </View>
-
-        <View style={Styles.boxes}>
-        <View style={Styles.box1}>
-          <View style={Styles.firstBox}>
-            <Wallet color="green" fill="green"/>
-            <Text>Wallet</Text>
+        
+        <View style={Styles.allBoxes}>
+          <View style={Styles.row}>
+            <View style={Styles.box}>
+              <View style={Styles.firstBox}>
+                <Wallet color="green" fill="green" />
+                <Text>Wallet</Text>
+              </View>
+              <Text style={Styles.firstBoxText}>
+                You can deposit this{"\n"}amount in Owealth{"\n"}and earn daily{"\n"}interests
+              </Text>
+              <Text style={Styles.punctuations}>--.--</Text>
             </View>
-          <Text style={Styles.firstBoxText}>You can deposit this{"\n"}amount in Owealth{"\n"}and earn daily{"\n"}interests</Text>
 
-        </View>
-        <View style={Styles.box2}>
+            <View style={[Styles.box, Styles.boxAlt]}>
+              <View style={Styles.firstBox}>
+                <CircleDollarSign color="purple" />
+                <Text>OWealth</Text>
+              </View>
+              <Text style={Styles.firstBoxText}>
+                Your daily returns. You{"\n"}can withdraw at any{"\n"}time
+              </Text>
+              <Text style={Styles.punctuations}>--.--</Text>
+            </View>
+          </View>
 
+          <View style={Styles.row2}>
+            <View style={Styles.box1}>
+              <View style={Styles.firstBox}>
+                <FontAwesome6 name="bullseye" color="#8a48f5ff" fill="green" size={22} />
+                <Text>Targets</Text>
+              </View>
+              <Text style={Styles.firstBoxText2}>
+                Save daily, weekly, or{"\n"}monthly towards your{"\n"}goal
+              </Text>
+              
+            </View>
+
+            <View style={Styles.box1}>
+              <View style={Styles.firstBox}>
+                <FontAwesome6 name="piggy-bank" color="#8a48f5ff" size={22}/>
+                <Text>SafeBox</Text>
+              </View>
+              <Text style={Styles.firstBoxText2}>
+                Your daily, weekly or{"\n"}monthly automatic{"\n"}savings
+              </Text>
+            </View>
+          </View>
+
+          <View style={Styles.row3}>
+            <View style={Styles.box1}>
+              <View style={Styles.firstBox}>
+                <FontAwesome6 name="bag-shopping" color="#8a48f5ff" size={22} fill="green" />
+                <Text>Fixed</Text>
+              </View>
+              <Text style={Styles.firstBoxText2}>
+                Min: #1000{"\n"}Tenor: 7-1000 days
+              </Text>
+              
+            </View>
+
+            <View style={Styles.box1}>
+              <View style={Styles.firstBox}>
+                <FontAwesome6 name="sack-dollar" size={22} color="#8a48f5ff" />
+                <Text>Spend & Save</Text>
+              </View>
+              <Text style={Styles.firstBoxText2}>
+                Your percentage{"\n"}savings every time you{"\n"}spend or transfer
+              </Text>
+            </View>
+          </View>
+
+          <View style={Styles.row4}>
+            <View style={Styles.box4}>
+              <View style={Styles.firstBox4}>
+                <FontAwesome6 name="chart-line" size={18} color="#8a48f5ff" fill="green" />
+                <Text>Savings Report</Text>
+              </View>
+              <Text style={Styles.firstBoxText2}>
+                Check out your{"\n"}savings journey so far
+              </Text>
+              
+            </View>
+
+            
+          </View>
         </View>
-        </View>
+
 
       </View>
+      </ScrollView>
     </SafeAreaView>
-    
   );
 }
 
@@ -53,16 +122,20 @@ const Styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: "white",
-    padding: 16,
+    paddingHorizontal: 16,
+    paddingTop: 20,
   },
 
   container: {
     flex: 1,
-    
+  },
+
+  scrollContainer: {
+    paddingBottom: 20,
   },
 
   headerTab: {
-    marginTop: 30,
+    marginTop: 20,
     marginBottom: 20,
     flexDirection: "row",
     justifyContent: "space-between",
@@ -74,10 +147,10 @@ const Styles = StyleSheet.create({
     fontWeight: "600",
   },
 
-  balaceContainer: {
+  balanceContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    
+    marginBottom: 10,
   },
 
   leftContent: {
@@ -86,7 +159,6 @@ const Styles = StyleSheet.create({
 
   rightContent: {
     gap: 2,
-
   },
 
   star: {
@@ -98,43 +170,102 @@ const Styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "900",
     textAlign: "right",
+    color: "green",
   },
 
-  boxes: {
+  allBoxes: {
+    justifyContent: "center",
+  },
+
+  row: {
     flexDirection: "row",
-    marginTop: 30,
-    gap: 10,
+    justifyContent: "space-between",
+    marginTop: 15,
+    marginBottom: 10,
+  },
+
+  row2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 15,
+    marginBottom: 10,
+  },
+
+  box: {
+    flex: 1,
+    backgroundColor: "#dff9e0ff",
+    borderRadius: 10,
+    padding: 14,
+    height: 170,
+    justifyContent: "space-between",
+    marginHorizontal: 5, 
   },
 
   box1: {
-    flex: 1,                             
-    backgroundColor: "#dff9e0ff",
-    padding: 20,
+    flex: 1,
+    backgroundColor: "#acb3ad3b",
     borderRadius: 10,
-    height: 150,
-    
+    padding: 14,
+    height: 170,
+    marginHorizontal: 5, 
   },
 
-  box2: {
-    flex: 1,                             
+  boxAlt: {
     backgroundColor: "#f1dff9ff",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    height: 150,
   },
 
   firstBox: {
     flexDirection: "row",
-    gap: 10,
     alignItems: "center",
-    
-
+    gap: 10,
   },
 
   firstBoxText: {
-    marginTop: 10,
-    fontSize: 16,
+    fontSize: 12,
     color: "#626060ff",
+    
   },
+
+  firstBoxText2: {
+    fontSize: 12,
+    color: "#626060ff",
+    marginTop: 10,
+  },
+
+  punctuations: {
+    fontSize: 20,
+    fontWeight: "800",
+    textAlign: "left",
+  },
+
+  row3: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 15,
+    marginBottom: 10,
+  },
+
+  row4: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 15,
+    marginBottom: 10,
+  },
+
+  box4: {
+    backgroundColor: "#acb3ad3b",
+    borderRadius: 10,
+    padding: 14,
+    height: 170,
+    marginHorizontal: 5, 
+    width: "47%",
+  },
+
+  firstBox4: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+
+  
 });
